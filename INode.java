37db68a45d8e8296ef872b90731eb25fbe127c73
pratikshabathe <pratikshabathe11@gmail.com>
2022-12-05 08:51:12 +0530

    Create new files

---
+++ INode.java
@@ -0,0 +1,9 @@
+package com.blz.day15;
+
+public interface INode<K> {
+
+	  K getKey();
+
+	  void setKey(K key);
+
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.blz.day15;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+	    myBinaryTree.add(56);
+	    myBinaryTree.add(30);
+	    myBinaryTree.add(70);
+
+	    int size = myBinaryTree.getSize();
+	    System.out.println("size = " + size); 
+	}
+}
